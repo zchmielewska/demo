d3993ddb96b7ebf373e1bb7f82a459e056814f70
Zuzanna <zchmielewska@gmail.com>
2025-12-09 14:00:20 +0100

    Update app to run on host 0.0.0.0 and port 8080

--- app/app.py
+++ app/app.py
@@ -11,10 +11,11 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), "data", "foos.pkl")
 with open(DATA_PATH, "rb") as f:
     FOOS = pickle.load(f)
 
+
 @app.route("/")
 def index():
     return render_template("index.html", foos=FOOS[1:10])
 
 
-# if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
